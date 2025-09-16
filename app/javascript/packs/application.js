ad16300ab68faeebf9c9a589c58e628f5c3d1991
iron <i30bany.pino1100@gmail.com>
2025-09-16 20:19:16 +0900

    初回提出

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,31 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+import '../stylesheets/_reset.scss'
+import '../images/airplane.png';
+import '../images/login.png';
+import '../images/SAYAPAKU4745_TP_V.png';
+import '../images/skytree-04_TP_V.png';
+import '../images/osaka.png';
+import '../images/byoudouinjpg.png'
+import '../images/hokaido.png'
+import '../images/pencil.svg'
+import '../images/point.svg'
+import '../images/diamond.svg'
+import '../images/location.svg'
+import "../stylesheets/application.scss";
+import "./_common.js"
+import "../stylesheets/_common_footer.scss"
+import "../stylesheets/_common_body.scss"
+import "../stylesheets/_common_header.scss"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
